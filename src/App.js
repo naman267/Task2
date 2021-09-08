0093b234320e8d7560507ff131cf0fa8ff0b6b53
Naman Jain <namanb139j@gmail.com>
2021-09-08 16:53:18 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -10,22 +10,22 @@ function App() {
       <div className="container mt-5 d-flex justify-content-center">
         <div className="card p-3">
           <div className="d-flex align-items-center">
-            <div className="image"> <img src={user} class="rounded" width="155" /> </div>
+            <div className="image"> <img src={user} className="rounded" width="155" /> </div>
             <div className="ml-3 w-100">
-              <h4 className="mb-0 mt-0">Alex Morrision</h4>
-              <span>Senior Journalist</span>
+              <h4 className="left mb-0 mt-0">Alex Morrision</h4>
+              <p className="left pp">Senior Journalist</p>
               <div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
                 <div className="d-flex flex-column">
                   <span className="articles">Articles</span>
-                  <span className="number1">38</span>
+                  <span className="left number1">38</span>
                 </div>
                 <div className="d-flex flex-column">
                   <span className="followers">Followers</span>
-                  <span className="number2">980</span>
+                  <span className="left number2">980</span>
                 </div>
                 <div className="d-flex flex-column">
                   <span className="rating">Rating</span>
-                  <span className="number3">8.9</span>
+                  <span className="left number3">8.9</span>
                 </div>
               </div>
               <div className="button mt-2 d-flex flex-row align-items-center">
